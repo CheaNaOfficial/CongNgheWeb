1b263dced79ea318a14a60792e7f6105e5907757
CheaNaOfficial <mathk7659@gmail.com>
2023-01-25 13:55:37 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,62 @@
+import arthur from './img/arthur.png';
+import batman from './img/batman.png';
+import blackpanter from './img/blackpanter.png';
+import kashima from './img/kashima.png';
+
+
+
+
+const data ={
+    cardData:[
+        {
+            id:1,
+            img:arthur,
+            title: ' title1',
+            desc: ' desc1'
+        },
+        {
+            id:2,
+            img: batman,
+            title: ' title2',
+            desc: ' desc2'
+        },
+        {
+            id:3,
+            img: blackpanter,
+            title: ' title3',
+            desc: ' desc3'
+        },
+        {
+            id:4,
+            img: kashima,
+            title: ' title4',
+            desc: ' desc4'
+        },
+        {
+            id:5,
+            img:arthur,
+            title: 'title5',
+            desc: ' desc5'
+        },
+        {
+            id:6,
+            img: batman,
+            title: ' title6',
+            desc: ' desc6'
+        },
+        {
+            id:7,
+            img: blackpanter,
+            title: 'title7',
+            desc: ' desc7'
+        },
+        {
+            id:8,
+            img: kashima,
+            title: 'title7',
+            desc: ' desc8'
+        }
+        
+    ]
+}
+export default data;
